4964abeea5d1411268ed66b2c6d222225fc74b73
Your Name <you@example.com>
2020-04-21 19:25:00 +0200

    db_mp

--- main/main.c
+++ main/main.c
@@ -3,9 +3,5 @@
 int main()
 {
     int list[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    db_m_print("cesi est un stringue rose", "-s *-*");
-    // db_mp_write_put_base(LONG_MIN, BASE10);
-    // char c = 31;
-    // write(1, &c, 1);
-    // db_mp_write_char(&c, 1);
+    db_m_print(list, "-i 10*-*");
 }
